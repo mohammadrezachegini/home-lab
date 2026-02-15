3adc7ecfdddc153e4d94124e6875c2705f93d2ce
Reza <mohammadreza.chegini01@gmail.com>
2026-02-14 20:47:33 -0800

    Added docker compose and ansible script structure

---
+++ fullstack-gitops-argocd/ansible-st.sh
@@ -0,0 +1,5 @@
+mkdir -p ansible/inventory
+mkdir -p ansible/roles/k3s/{defaults,files,handlers,meta,tasks,templates,vars}
+mkdir -p ansible/roles/argocd/{defaults,files,handlers,meta,tasks,templates,vars}
+mkdir -p ansible/roles/monitoring/{defaults,files,handlers,meta,tasks,templates,vars}
+mkdir -p ansible/roles/blog-seed/{defaults,files,handlers,meta,tasks,templates,vars}
